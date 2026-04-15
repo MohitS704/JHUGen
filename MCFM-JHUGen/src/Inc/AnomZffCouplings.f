457672b8ff7dcd86546c2720591405e1820bd26e
Mohit Srivastav <vir.moh@gmail.com>
2026-04-15 18:18:34 -0400

    Revert "Ztt couplings"
    This reverts commit 668dfe9c7c393462dc70688f38f13e5bab918af6.
    
    yy#

--- MCFM-JHUGen/src/Inc/AnomZffCouplings.f
+++ MCFM-JHUGen/src/Inc/AnomZffCouplings.f
@@ -2,12 +2,11 @@
 
         double precision reZ,leZ,lnZ,rnZ
         double precision rquZ,lquZ,rqdZ,lqdZ 
-        double precision clanou,cranou,clanod,cranod,clanot,cranot
+        double precision clanou,cranou,clanod,cranod
 
         common/AnomZffCouplings/
      & AllowAnomalousZffCouplings,
 
      & reZ,leZ,lnZ,rnZ,
      & rquZ,lquZ,rqdZ,lqdZ,
-     & clanou,cranou,clanod,cranod,
-     & clanot,cranot
+     & clanou,cranou,clanod,cranod
